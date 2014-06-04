ad97d5f9beb1d39cc662a3b7f026a0aa6c119c3a
ejo090 <ejo090.403forbidden@gmail.com>
2014-06-04 16:48:28 +0900

    add html file and fixed MyVaadinUI.java

--- rootbeer-rbms/rootbeer-rbms-view/src/main/java/org/rootbeer/rbms/view/MyVaadinUI.java
+++ rootbeer-rbms/rootbeer-rbms-view/src/main/java/org/rootbeer/rbms/view/MyVaadinUI.java
@@ -8,7 +8,10 @@ import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.Panel;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
@@ -17,24 +20,41 @@ import com.vaadin.ui.VerticalLayout;
 public class MyVaadinUI extends UI
 {
 
-    @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "org.rootbeer.rbms.view.AppWidgetSet")
-    public static class Servlet extends VaadinServlet {
-    }
+	@WebServlet(value = "/*", asyncSupported = true)
+	@VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class)
+	public static class Servlet extends VaadinServlet {
+	}
 
-    @Override
-    protected void init(VaadinRequest request) {
-        final VerticalLayout layout = new VerticalLayout();
-        layout.setMargin(true);
-        setContent(layout);
-        
-        Button button = new Button("Click Me");
-        button.addClickListener(new Button.ClickListener() {
-            public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
-            }
-        });
-        layout.addComponent(button);
-    }
+	@Override
+	protected void init(VaadinRequest request) {
+		final VerticalLayout layout = new VerticalLayout();
+		layout.setMargin(true);
+		setContent(layout);
+
+		// Have a Panel where to put the custom layout.
+		Panel panel = new Panel("Login");
+		panel.setSizeUndefined();
+		layout.addComponent(panel);
+
+		// Create custom layout from "layoutname.html" template.
+		CustomLayout custom = new CustomLayout("login");
+		custom.addStyleName("customlayoutexample");
+
+		// Use it as the layout of the Panel.
+		panel.setContent(custom);
+
+		// Create a few components and bind them to the location tags
+		// in the custom layout.
+		TextField username = new TextField();
+		custom.addComponent(username, "username");
+
+
+		TextField password = new TextField();
+		custom.addComponent(password, "password");
+
+
+		Button ok = new Button("Login");
+		custom.addComponent(ok, "okbutton");
+	}
 
 }
