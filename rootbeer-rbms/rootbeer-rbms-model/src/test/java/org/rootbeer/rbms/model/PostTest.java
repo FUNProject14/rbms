8f01884ec3f5f5802ac2a4dd8edde0bc7624ff73
funproject14 <funproject14@Macintosh.local>
2014-06-06 01:46:44 +0900

    Revise UserTest's testEquality

--- rootbeer-rbms/rootbeer-rbms-model/src/test/java/org/rootbeer/rbms/model/PostTest.java
+++ rootbeer-rbms/rootbeer-rbms-model/src/test/java/org/rootbeer/rbms/model/PostTest.java
@@ -34,15 +34,46 @@ public class PostTest {
 	
 	@Test
 	public void testEquality(){
-		Date testDate = new Date();
-		Post testPostA = new Post("TEST1", "uchiyama", testDate, "parent");
-		Post testPostB = new Post("TEST1", "uchiyama", testDate, "parent");
-		Post testPostC = new Post("TEST", "uchiyama1", testDate, "parent");
+		long dateA = System.currentTimeMillis();
+		long dateB = System.currentTimeMillis()+1;
+		Date testDateA = new Date(dateA);
+		Date testDateB = new Date(dateB);
+		Post testPostAAAA = new Post("TESTA", "uchiyamaA", testDateA, "parentA");
+	    Post testPostAAAA2 = new Post("TESTA", "uchiyamaA", testDateA, "parentA");
+		Post testPostAAAB = new Post("TESTA", "uchiyamaA", testDateA, "parentB");
+		Post testPostAABA = new Post("TESTA", "uchiyamaA", testDateB, "parentA");
+		Post testPostAABB = new Post("TESTA", "uchiyamaA", testDateB, "parentB");
+		Post testPostABAA = new Post("TESTA", "uchiyamaB", testDateA, "parentA");
+		Post testPostABAB = new Post("TESTA", "uchiyamaB", testDateA, "parentB");
+		Post testPostABBA = new Post("TESTA", "uchiyamaB", testDateB, "parentA");
+		Post testPostABBB = new Post("TESTA", "uchiyamaB", testDateB, "parentB");
+		Post testPostBAAA = new Post("TESTB", "uchiyamaA", testDateA, "parentA");
+		Post testPostBAAB = new Post("TESTB", "uchiyamaA", testDateA, "parentB");
+		Post testPostBABA = new Post("TESTB", "uchiyamaA", testDateB, "parentA");
+		Post testPostBABB = new Post("TESTB", "uchiyamaA", testDateB, "parentB");
+		Post testPostBBAA = new Post("TESTB", "uchiyamaB", testDateA, "parentA");
+		Post testPostBBAB = new Post("TESTB", "uchiyamaB", testDateA, "parentB");
+		Post testPostBBBA = new Post("TESTB", "uchiyamaB", testDateB, "parentA");
+		Post testPostBBBB = new Post("TESTB", "uchiyamaB", testDateB, "parentB");	
 		
-		assertThat(testPostA, is(testPostB));
-		assertThat(testPostA, is(not(testPostC)));
+		assertThat(testPostAAAA, is(testPostAAAA2));
+		assertThat(testPostAAAA, is(not(testPostAAAB)));
+		assertThat(testPostAAAA, is(not(testPostAABA)));
+		assertThat(testPostAAAA, is(not(testPostAABB)));
+		assertThat(testPostAAAA, is(not(testPostABAA)));
+		assertThat(testPostAAAA, is(not(testPostABAB)));
+		assertThat(testPostAAAA, is(not(testPostABBA)));
+		assertThat(testPostAAAA, is(not(testPostABBB)));
+		assertThat(testPostAAAA, is(not(testPostBAAA)));
+		assertThat(testPostAAAA, is(not(testPostBAAB)));
+		assertThat(testPostAAAA, is(not(testPostBABA)));
+		assertThat(testPostAAAA, is(not(testPostBABB)));
+		assertThat(testPostAAAA, is(not(testPostBBAA)));
+		assertThat(testPostAAAA, is(not(testPostBBAB)));
+		assertThat(testPostAAAA, is(not(testPostBBBA)));
+		assertThat(testPostAAAA, is(not(testPostBBBB)));
 		
-		assertThat(testPostA.hashCode(), is(testPostB.hashCode()));
-		assertThat(testPostA.hashCode(), is(not(testPostC.hashCode())));
+		assertThat(testPostAAAA.hashCode(), is(testPostAAAA2.hashCode()));
+		assertThat(testPostAAAA.hashCode(), is(not(testPostBBBB.hashCode())));
 	}
 }
