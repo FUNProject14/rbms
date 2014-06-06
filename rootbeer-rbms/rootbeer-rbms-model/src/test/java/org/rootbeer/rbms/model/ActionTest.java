469aec07306ab74ff652779699833fc113a248df
uryu1994 <b1012086@fun.ac.jp>
2014-06-06 10:45:13 +0900

    Revise Actiontest's testEquality

--- rootbeer-rbms/rootbeer-rbms-model/src/test/java/org/rootbeer/rbms/model/ActionTest.java
+++ rootbeer-rbms/rootbeer-rbms-model/src/test/java/org/rootbeer/rbms/model/ActionTest.java
@@ -10,48 +10,62 @@ import org.junit.Test;
 import org.rootbeer.rbms.model.Action.Act;
 
 public class ActionTest {
-	
+
 	@Test
 	public void testContstructor(){
-		Date testDate = new Date(System.currentTimeMillis());
-		Action testAction = new Action(Act.BUY, "mahara123", testDate);
-		
+
+		Action testAction = new Action(Act.BUY, "hogeA", new Date(System.currentTimeMillis()));
+
 		assertThat(testAction.getAct(), is(Act.BUY));
-		assertThat(testAction.getActorUserId(), is("mahara123"));
-		assertThat(testAction.getActedTime(), is(testDate));
-		
+		assertThat(testAction.getActorUserId(), is("hogeA"));
+		assertThat(testAction.getActedTime(), is(new Date(System.currentTimeMillis())));
+
 	}
-	
+
+
 	@Test
 	public void testAccessor(){
+
 		Action testAction = new Action();
-		
+
 		testAction.setAct(Act.BUY);
 		assertThat(testAction.getAct(), is(Act.BUY));
 		assertThat(testAction.getAct(), is(not(Act.DRINK)));
-		
+
 		testAction.setActorUserId("hogehoge");
 		assertThat(testAction.getActorUserId(), is("hogehoge"));
-		
-		Date testDate = new Date(System.currentTimeMillis());
-		testAction.setActedTime(testDate);
-		assertThat(testAction.getActedTime(), is(testDate));
-		
+
+		testAction.setActedTime(new Date(System.currentTimeMillis()));
+		assertThat(testAction.getActedTime(), is(new Date(System.currentTimeMillis())));
+
 	}
-	
+
+
 	@Test
 	public void testEquality(){
-		
-		Date testDate = new Date(System.currentTimeMillis());
-		
-		Action actionA = new Action(Act.BUY, "hoge1", testDate);
-		Action actionB = new Action(Act.BUY, "hoge1", testDate);
-		Action actionC = new Action(Act.BUY,"hoge2", testDate);
-		
-		assertThat(actionA,  is(actionB));
-		assertThat(actionA, is(not(actionC)));
-		assertThat(actionA.hashCode(), is(actionB.hashCode()));
-		
+
+		Action testActionAAA = new Action(Act.BUY, "hogeA", new Date(System.currentTimeMillis()));
+		Action testActionAAA2 = new Action(Act.BUY, "hogeA", new Date(System.currentTimeMillis()));
+		Action testActionAAB = new Action(Act.BUY, "hogeA", new Date(System.currentTimeMillis()+1));
+		Action testActionABA = new Action(Act.BUY, "hogeB", new Date(System.currentTimeMillis()));
+		Action testActionBAA = new Action(Act.DRINK, "hogeA", new Date(System.currentTimeMillis()));
+		Action testActionABB = new Action(Act.BUY, "hogeB", new Date(System.currentTimeMillis()+1));
+		Action testActionBAB = new Action(Act.DRINK, "hogeA", new Date(System.currentTimeMillis()+1));
+		Action testActionBBA = new Action(Act.DRINK, "hogeB", new Date(System.currentTimeMillis()));
+		Action testActionBBB = new Action(Act.DRINK, "hogeB", new Date(System.currentTimeMillis()+1));
+
+		assertThat(testActionAAA, is(testActionAAA2));
+		assertThat(testActionAAA, is(not(testActionAAB)));
+		assertThat(testActionAAA, is(not(testActionABA)));
+		assertThat(testActionAAA, is(not(testActionBAA)));
+		assertThat(testActionAAA, is(not(testActionABB)));
+		assertThat(testActionAAA, is(not(testActionBAB)));
+		assertThat(testActionAAA, is(not(testActionBBA)));
+		assertThat(testActionAAA, is(not(testActionBBB)));
+
+		assertThat(testActionAAA.hashCode(), is(testActionAAA2.hashCode()));
+		assertThat(testActionAAA.hashCode(), is(not(testActionAAB.hashCode())));
+
 	}
-	
+
 }
