c90e489d03ea5e8982d43412e3acf3aa0b29e5b1
yuuto1012227 <b1012227@fun.ac.jp>
2014-06-15 00:52:57 +0900

    judge to exist userID or not and add new user info in USER Backet when judgement is true
    Add CreateAccountException & Add API create new account in UserManagement

--- rootbeer-rbms/rootbeer-rbms-model/src/main/java/org/rootbeer/rbms/logic/UserManagement.java
+++ rootbeer-rbms/rootbeer-rbms-model/src/main/java/org/rootbeer/rbms/logic/UserManagement.java
@@ -2,10 +2,13 @@ package org.rootbeer.rbms.logic;
 
 import com.couchbase.client.CouchbaseClient;
 import com.google.gson.Gson;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.rootbeer.rbms.model.*;
 
 import static org.rootbeer.rbms.util.Database.*;
 
-import org.rootbeer.rbms.model.*;
 import org.rootbeer.rbms.util.Database.Bucket;
 import org.rootbeer.rbms.util.ModelUtil;
 
@@ -17,17 +20,31 @@ public final class UserManagement {
     }
     
     /**
-     * 新規アカウントの作成をします。
+     * 新規アカウントの作成（パスワード未設定）
      * @param newUserId 新規登録するユーザID
-     * @param newPassword 新規登録するパスワード
      * @param newFullName 新規登録するフルネーム
+     * @throws org.rootbeer.rbms.logic.CreateAccountException
      */
-    public static void CreateNewAccount(String newUserId, String newPassword, String newFullName){
-        getClient(Bucket.USER);
-        User newUser = new User(newUserId, newPassword, newFullName);
-        CouchbaseClient client = getClient(Bucket.USER);
-        Gson userGson = ModelUtil.GSON;
+    public static void CreateNewAccount(String newUserId, String newFullName) throws CreateAccountException{
+        User newUser = new User(newUserId, "", newFullName);
+        User existUser = getUser(newUserId); 
+        Pattern p = Pattern.compile("^\\w{1,16}$");
         
-        client.add(newUserId, userGson.toJson(newUser));
+        // 重複するユーザIDが存在する場合、例外処理
+        if(newUser.equals(existUser)){
+            throw new ConflictedUserIdException();    
+        }
+        
+        // ユーザIDが半角英数字で作成されていない, 空文字("")、null文字が含まれている, 1~16文字以内で書かれていない場合、例外処理
+        try{
+            Matcher m = p.matcher(newUser.getUserId());
+            if(!m.find()){
+                throw new InvaliedUserIdException();
+            }
+        }catch(NullPointerException e){
+            throw new InvaliedUserIdException();
+        } 
+        
+        addUser(newUserId, newUser);
     }
 }
