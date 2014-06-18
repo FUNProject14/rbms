f25148e7cc676ea09d675791b7742b96a96cba8b
yuuto1012227 <azuroma0623@gmail.com>
2014-06-18 19:16:08 +0900

    create new account method

--- rootbeer-rbms/rootbeer-rbms-model/src/main/java/org/rootbeer/rbms/logic/CreateAccountException.java
+++ rootbeer-rbms/rootbeer-rbms-model/src/main/java/org/rootbeer/rbms/logic/CreateAccountException.java
@@ -1,28 +1,30 @@
 package org.rootbeer.rbms.logic;
 
-public class CreateAccountException extends Exception{
-    public CreateAccountException(String str){    
-    super(str);
-    }
-}
+public class CreateAccountException extends Exception {
 
-/**
- * ユーザIDが半角英数字で作成されていない
- * ユーザIDに空文字、null文字が含まれている
- * ユーザIDが17文字以上で書かれている
- * ときに例外処理
- */
-class InvaliedUserIdException extends CreateAccountException{
-    public InvaliedUserIdException() {
-        super("ユーザIDが無効です。使用できるのは1文字以上16文字以下の半角英数字のみです。");
+    public CreateAccountException(String str) {
+        super(str);
+    }
+
+    /**
+     * ユーザIDが半角英数字で作成されていない 
+     * ユーザIDに空文字、null文字が含まれている 
+     * ユーザIDが17文字以上で書かれている 
+     * ときに例外処理
+     */
+    public static class InvalidUserId extends CreateAccountException {
+        public InvalidUserId() {
+            super("ユーザIDが無効です。使用できるのは1文字以上16文字以下の半角英数字のみです。");
+        }
     }
 }
 
 /**
  * 重複するユーザIDが存在するとき例外処理
  */
-class ConflictedUserIdException extends CreateAccountException{
+class ConflictedUserIdException extends CreateAccountException {
+
     public ConflictedUserIdException() {
         super("既に存在するユーザIDです。");
-    }   
+    }
 }
--- rootbeer-rbms/rootbeer-rbms-model/src/main/java/org/rootbeer/rbms/logic/UserManagement.java
+++ rootbeer-rbms/rootbeer-rbms-model/src/main/java/org/rootbeer/rbms/logic/UserManagement.java
@@ -1,17 +1,8 @@
 package org.rootbeer.rbms.logic;
 
-import com.couchbase.client.CouchbaseClient;
-import com.google.gson.Gson;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.rootbeer.rbms.model.*;
 
 import static org.rootbeer.rbms.util.Database.*;
-
-import org.rootbeer.rbms.util.Database.Bucket;
-import org.rootbeer.rbms.util.ModelUtil;
-
 /**
  * ユーザ関連のクラスです。
  */
@@ -25,26 +16,22 @@ public final class UserManagement {
      * @param newFullName 新規登録するフルネーム
      * @throws org.rootbeer.rbms.logic.CreateAccountException
      */
-    public static void CreateNewAccount(String newUserId, String newFullName) throws CreateAccountException{
-        User newUser = new User(newUserId, "", newFullName);
-        User existUser = getUser(newUserId); 
-        Pattern p = Pattern.compile("^\\w{1,16}$");
-        
+    public static void createNewAccount(String newUserId, String newFullName) throws CreateAccountException{
+        assert newUserId != null;
+        assert newFullName != null;
+             
         // 重複するユーザIDが存在する場合、例外処理
-        if(newUser.equals(existUser)){
+        User existUser = getUser(newUserId);
+        if(existUser != null){
             throw new ConflictedUserIdException();    
         }
         
-        // ユーザIDが半角英数字で作成されていない, 空文字("")、null文字が含まれている, 1~16文字以内で書かれていない場合、例外処理
-        try{
-            Matcher m = p.matcher(newUser.getUserId());
-            if(!m.find()){
-                throw new InvaliedUserIdException();
-            }
-        }catch(NullPointerException e){
-            throw new InvaliedUserIdException();
-        } 
+        // ユーザIDが半角英数字で作成されていない, 空文字("")、null文字が含まれている, 1~16文字以内で書かれていない場合、例外処理   
+        if(User.isValidUserId(newUserId)){
+            throw new CreateAccountException.InvalidUserId();
+        }
         
+        User newUser = new User(newUserId, "", newFullName);   
         addUser(newUserId, newUser);
     }
 }
