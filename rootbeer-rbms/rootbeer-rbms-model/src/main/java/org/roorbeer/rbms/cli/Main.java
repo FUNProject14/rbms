64b8bcd1eb31d1bffb946d67fa276ad00e3265c9
Choco <b1012181@gmail.com>
2014-06-27 17:06:17 +0900

    add case of getposts

--- rootbeer-rbms/rootbeer-rbms-model/src/main/java/org/roorbeer/rbms/cli/Main.java
+++ rootbeer-rbms/rootbeer-rbms-model/src/main/java/org/roorbeer/rbms/cli/Main.java
@@ -1,10 +1,17 @@
 package org.roorbeer.rbms.cli;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+
 import org.rootbeer.rbms.logic.CreateAccountException;
+
 import static org.rootbeer.rbms.logic.UserManagement.createNewAccount;
+
 import org.rootbeer.rbms.model.*;
 import org.rootbeer.rbms.model.Action.Act;
+
 import static org.rootbeer.rbms.util.Database.*;
 
 public class Main {
@@ -29,6 +36,32 @@ public class Main {
             	addAction(actionDrink);
             	break;
             case "getposts":
+            	String[] userIDs = getUserIDs();
+            	ArrayList<Action> userActions = new ArrayList<Action>();
+            	for(String userID : userIDs){
+            		Action[] actions = getActions(userID);
+            		if(actions != null)
+            			userActions.addAll(Arrays.asList(actions));
+            	}
+            	
+            	//TODO:userActionsを時刻の逆順にソートする
+            	
+            	for(Action userAction : userActions){
+            		StringBuilder b = new StringBuilder();
+            		b.append(userAction.getActorUserId() + "が");
+            		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy年MM月dd日HH時mm分ss秒");
+            		b.append(sdf1.format(userAction.getActedTime()) + "に");
+            		switch(userAction.getAct()){
+            		case BUY :
+            			b.append("買いました");
+            			break;
+            		case DRINK :
+            			b.append("飲みました");
+            			break;
+            		}
+            		System.out.println(b);
+            	}
+            	break;
         }
     }
 }
