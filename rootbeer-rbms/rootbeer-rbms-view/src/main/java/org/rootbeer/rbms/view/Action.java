bad0b860bd575cafbf7f62fcbb32aac364c96f12
ejo090 <ejo090.403forbidden@gmail.com>
2014-07-08 16:19:33 +0900

    CREATE GAWA

--- rootbeer-rbms/rootbeer-rbms-view/src/main/java/org/rootbeer/rbms/view/Action.java
+++ rootbeer-rbms/rootbeer-rbms-view/src/main/java/org/rootbeer/rbms/view/Action.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import javax.servlet.annotation.WebServlet;
 import org.rootbeer.rbms.logic.ActionManagement;
 import org.rootbeer.rbms.logic.AddActionIllegalStateException;
+import org.rootbeer.rbms.util.Database;
 
 // import org.rootbeer.rbms.model.*;
 
@@ -17,6 +18,10 @@ import org.rootbeer.rbms.logic.AddActionIllegalStateException;
 @Title("Hello Window")
 public class Action extends UI {
     
+    Table actionLogTable = new Table("ACTION LOG");
+    Label remainRootBeerLabel = new Label();
+
+
     @WebServlet(value = "/*", asyncSupported = true)
     @VaadinServletConfiguration(productionMode = false, ui = Action.class)
     public static class Servlet extends VaadinServlet {
@@ -24,14 +29,23 @@ public class Action extends UI {
     
     @Override
     protected void init(VaadinRequest request) {
+        
+        // Initialize Table
+        actionLogTable.addContainerProperty("UserID", String.class, null);
+        actionLogTable.addContainerProperty("Act", String.class, null);
+        actionLogTable.addContainerProperty("Date", String.class, null);
+        
+        // refresh table
+        refreshActionLogTable("michiko2");
+        refreshRemainRootBeer("michiko2");
+        
         // Create the content root layout for the UI
         VerticalLayout content = new VerticalLayout();
         setContent(content);
 
-        // Display the greeting
-        content.addComponent(new Label("Hello World!"));
         
-        // 
+        
+        // Drink rootbeer!
         final Button drinkButton = new Button("Drink");
         drinkButton.addClickListener(new Button.ClickListener() {
 
@@ -41,12 +55,49 @@ public class Action extends UI {
                         org.rootbeer.rbms.model.Action.Act.DRINK, "michiko2", new Date());
                 try {
                     ActionManagement.addActionWithChecking(action);
+                    refreshActionLogTable("michiko2");
+                    refreshRemainRootBeer("michiko2");
                 } catch (AddActionIllegalStateException e) {
                     drinkButton.setComponentError(new UserError(e.getMessage()));
                 }
             }
         });
         
+        // Buy rootbeer!
+        final Button buyButton = new Button("Buy");
+        buyButton.addClickListener(new Button.ClickListener() {
+
+            @Override
+            public void buttonClick(Button.ClickEvent event) {
+                org.rootbeer.rbms.model.Action action = new org.rootbeer.rbms.model.Action(
+                        org.rootbeer.rbms.model.Action.Act.BUY, "michiko2", new Date());
+                try {
+                    ActionManagement.addActionWithChecking(action);
+                    refreshActionLogTable("michiko2");
+                    refreshRemainRootBeer("michiko2");
+                } catch (AddActionIllegalStateException e) {
+                    buyButton.setComponentError(new UserError(e.getMessage()));
+                }
+            }
+        });
+        
+        // Display Components
+        content.addComponent(remainRootBeerLabel);
+        content.addComponent(buyButton);
         content.addComponent(drinkButton);
+        content.addComponent(actionLogTable);
+    }
+    
+    
+    
+    void refreshActionLogTable (String userId) {
+        actionLogTable.removeAllItems();
+        for(org.rootbeer.rbms.model.Action action : Database.getActions(userId) ){
+            actionLogTable.addItem(new Object[] {action.getActorUserId(), action.getAct().toString(), action.getActedTime().toString()}, null);
+        }
+    }
+    
+    void refreshRemainRootBeer (String userId) {
+        remainRootBeerLabel.setValue(String.valueOf(ActionManagement.countStock(userId)));
     }
 }
